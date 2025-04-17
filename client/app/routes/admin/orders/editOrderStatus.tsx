3c26fc6b6e2f197c7af3c03883591b0fdad14155
solitdio079 <solitdio079@gmail.com>
2025-04-18 00:50:00 +0300

    Add edit status action

--- client/app/routes/admin/orders/editOrderStatus.tsx
+++ client/app/routes/admin/orders/editOrderStatus.tsx
@@ -3,14 +3,18 @@ import type { Route } from "../+types/AdminRoot";
 import { redirect } from "react-router";
 
 export async function clientAction({request, params}:Route.ClientActionArgs){
-    const {id} = params 
     const formData = await request.formData()
+    const {id} = params 
+   
 
     try {
         const req = await fetch(serverUrl + `/orders/${id}`, {
             method: "PATCH",
             mode: "cors",
             credentials: "include",
+            headers: {
+                'Content-Type': 'application/JSON',
+              },
             body: JSON.stringify(Object.fromEntries(formData))
         })
         const response = await req.json()
