ac90b707844515aa044d59e1dd58059dcc6aee13
solitdio079 <solitdio079@gmail.com>
2025-04-21 17:18:08 +0300

    Fix carousel reinit single page

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   ssr: {
-    noExternal: ['react-datepicker'],
+    noExternal: ['react-datepicker','flyonui'],
   },
   optimizeDeps: {
     include: ['apexcharts','flyonui'],
