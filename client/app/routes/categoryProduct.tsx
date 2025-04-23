0a81aeadc181ebb01588de789026d7ffae696f08
solitdio079 <solitdio079@gmail.com>
2025-04-23 17:30:22 +0300

    Add category products 3

--- client/app/routes/categoryProduct.tsx
+++ client/app/routes/categoryProduct.tsx
@@ -33,11 +33,11 @@ export default function categoryProduct({loaderData}: Route.ComponentProps) {
           <img src={banner} className="w-full" />
           <div className="flex justify-center w-full h-full items-center absolute top-0 backdrop-grayscale-100">
             <h1 className="text-3xl lg:text-5xl font-extrabold text-secondary">
-              {firstProduct.category.name}
+              {firstProduct.category.name || "Categorie vide"}
             </h1>
           </div>
           <div className="absolute bottom-2.5 w-full px-10 font-bold lg:text-lg text-secondary">
-            <Link to="/">Accueil</Link> / {firstProduct.category.name}
+            <Link to="/">Accueil</Link> / {firstProduct.category.name || "Categorie vide"}
           </div>
         </div>
 
