3b50140bb981eae0a182ae1806bcdf4d96553921
solitdio079 <solitdio079@gmail.com>
2025-04-26 21:16:19 +0300

    Add error to cash failed

--- client/app/routes/cashCheckoutFailed.tsx
+++ client/app/routes/cashCheckoutFailed.tsx
@@ -4,7 +4,7 @@ export default function CartCheckoutFailed() {
   return (
     <div className="flex justify-center">
       <div className="card md:max-w-md">
-        <div className="card-alert alert alert-success alert-soft" role="alert">
+        <div className="card-alert alert alert-error alert-soft" role="alert">
           <span className="font-bold">Erreur:</span>
           Une erreure s'est produit!
         </div>
